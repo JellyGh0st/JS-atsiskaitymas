85cc02067186ce71201fb071fe6703d530ee3956
JellyGh0st <vsumerkach@gmail.com>
2023-04-11 08:56:57 +0300

    TASK1

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,35 @@ pamatyti jo pateikto svorio konvertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+
+const forma = document.querySelector('form');
+const output = document.querySelector('#output');
+
+forma.addEventListener('submit', (e) => {
+  e.preventDefault(); 
+
+  const kg = document.querySelector('#search').value;
+
+  if (isNaN(kg)) {
+    output.innerHTML = 'Įvestis nėra numeris'; 
+    return;
+  }
+
+  const lb = kg * 2.2046;
+  const g = kg / 0.0010000;
+  const oz = kg * 35.274;
+
+  const atsakymas = `
+    <p>${kg} kg yra lygu to:</p>
+    <p>${lb.toFixed(2)} (svarai)</p>
+    <p>${g.toFixed(2)} (gramai)<p>
+    <p>${oz.toFixed(2)} (uncijos)<p>
+  `;
+  output.innerHTML = atsakymas;
+});
+
+
+
+
+
+
