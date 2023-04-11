9d46c38610303fc25191ac619a1a406ecfa4a0ff
JellyGh0st <vsumerkach@gmail.com>
2023-04-11 12:19:32 +0300

    Task 10

--- Task 10/script.js
+++ Task 10/script.js
@@ -1,6 +1,8 @@
-/* ------------------------------ TASK 10 ---------------------------------------------------
-Naudodami modulius sutvarkykite užduoties "Task 10" esančius failus taip, kad veiktų žemiau pateiktos funkcijos.
--------------------------------------------------------------------------------------------- */
+import {composition}  from './modules/math/composition.js'
+import {division}  from './modules/math/division.js'
+import {multiplication}  from './modules/math/multiplication.js'
+import {substraction}  from './modules/math/subtraction.js'
+import {one, two, three, four, five} from './modules/numbers/numbers.js';
 
 let a = composition(one, four);
 let b = division(four, two);
@@ -11,3 +13,7 @@ console.log(a);
 console.log(b);
 console.log(c);
 console.log(d);
+
+/* ------------------------------ TASK 10 ---------------------------------------------------
+Naudodami modulius sutvarkykite užduoties "Task 10" esančius failus taip, kad veiktų žemiau pateiktos funkcijos.
+-------------------------------------------------------------------------------------------- */
