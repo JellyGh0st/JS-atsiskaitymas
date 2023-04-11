d661f89e7808d11195f0f3bfc532e9847549d8e5
JellyGh0st <vsumerkach@gmail.com>
2023-04-11 13:10:34 +0300

    Task 4

--- Task 4/script.js
+++ Task 4/script.js
@@ -10,3 +10,25 @@ bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'cars.json';
+const output = document.querySelector('#output');
+
+fetch(ENDPOINT)
+  .then(res => res.json())
+  .then(cars => {
+    cars.forEach(car => {
+      const brandCard = document.createElement('div');
+      brandCard.classList.add('brand-card');
+      const brandName = document.createElement('h2');
+      brandName.textContent = car.brand;
+      brandCard.appendChild(brandName);
+      const modelList = document.createElement('ul');
+      car.models.forEach(model => {
+        const modelItem = document.createElement('li');
+        modelItem.textContent = model;
+        modelList.appendChild(modelItem);
+      });
+      brandCard.appendChild(modelList);
+      output.appendChild(brandCard);
+    });
+  })
+  .catch(error => console.error(error));
